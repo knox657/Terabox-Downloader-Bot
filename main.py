230f40a625848ac9c64962b445a9c7b136d8bc19
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-05-17 08:39:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -144,7 +144,8 @@ async def start_with_token(m: UpdateNewMessage):
             [Button.url("Click here", url=link)],
             [Button.url('How to use the bot', url=TUT_VID)]
         ]
-        await m.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", buttons=btn, protect_content=False, quote=True)
+        await m.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad,
+        return await m.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", buttons=btn, protect_content=False, quote=True)
         return
 
     check_if = await is_user_on_chat(bot, "@Ultroid_Official", m.peer_id)
@@ -247,6 +248,224 @@ async def handle_message(m: UpdateNewMessage):
     except Exception as e:
         await m.reply(f"An error occurred while processing the link: {str(e)}")
 
+def convert_seconds(seconds):
+    return time.strftime('%H:%M:%S', time.gmtime(seconds))
+
+async def get_message(m: UpdateNewMessage):
+    asyncio.create_task(handle_message(m))
+
+async def handle_message(m: UpdateNewMessage):
+    url = get_urls_from_string(m.message.message)
+    if not url:
+        return await m.reply("Please enter a valid url.")
+    
+    check_if = await is_user_on_chat(bot, "@backupbackupbackup", m.sender_id)
+    if not check_if:
+        return await m.reply("Please join @backupbackupbackup then send me the link again.")
+    
+    check_if = await is_user_on_chat(bot, "@pompompom_bot_support", m.sender_id)
+    if not check_if:
+        return await m.reply("Please join @pompompom_bot_support then send me the link again.")
+    
+    is_spam = db.get(m.sender_id)
+    if is_spam and m.sender_id not in [6695586027]:
+        return await m.reply("You are spamming. Please wait a 1 minute and try again.")
+    
+    hm = await m.reply("Sending you the media wait...")
+    count = db.get(f"check_{m.sender_id}")
+    if count and int(count) > 5:
+        return await hm.edit("You are limited now. Please come back after 2 hours or use another account.")
+    
+    shorturl = extract_code_from_url(url)
+    if not shorturl:
+        return await hm.edit("Seems like your link is invalid.")
+    
+    fileid = db.get(shorturl)
+    if fileid:
+        try:
+            await hm.delete()
+        except:
+            pass
+        
+        await bot(ForwardMessagesRequest(
+            from_peer=PRIVATE_CHAT_ID,
+            id=[int(fileid)],
+            to_peer=m.chat_id,
+            drop_author=True,
+            background=True,
+            drop_media_captions=False,
+            with_my_score=True,
+        ))
+        db.set(m.sender_id, time.monotonic(), ex=60)
+        db.set(f"check_{m.sender_id}",
+        return await hm.edit("You are limited now. Please come back after 2 hours or use another account.")
+    
+    data = await get_data(url)
+    if not data:
+        return await hm.edit("Sorry! API is dead or maybe your link is broken.")
+    
+    db.set(m.sender_id, time.monotonic(), ex=60)
+    if (
+        not data["file_name"].endswith(".mp4") and
+        not data["file_name"].endswith(".mkv") and
+        not data["file_name"].endswith(".Mkv") and
+        not data["file_name"].endswith(".webm")
+    ):
+        return await hm.edit("Sorry! File is not supported for now. I can download only .mp4, .mkv and .webm files.")
+    
+    if int(data["sizebytes"]) > 524288000 and m.sender_id not in [6695586027]:
+        return await hm.edit(f"Sorry! File is too big. I can download only 500MB and this file is of {data['size']} .")
+    
+    start_time = time.time()
+    cansend = CanSend()
+
+    async def progress_bar(current_downloaded, total_downloaded, state="Sending"):
+        if not cansend.can_send():
+            return
+        
+        bar_length = 20
+        percent = current_downloaded / total_downloaded
+        arrow = "█" * int(percent * bar_length)
+        spaces = "░" * (bar_length - len(arrow))
+        
+        elapsed_time = time.time() - start_time
+        head_text = f"{state} `{data['file_name']}`"
+        progress_bar = f"[{arrow + spaces}] {percent:.2%}"
+        upload_speed = current_downloaded / elapsed_time if elapsed_time > 0 else 0
+        speed_line = f"Speed: **{get_formatted_size(upload_speed)}/s**"
+        time_remaining = (
+            (total_downloaded - current_downloaded) / upload_speed if upload_speed > 0 else 0
+        )
+        time_line = f"Time Remaining: `{convert_seconds(time_remaining)}`"
+        size_line = f"Size: **{get_formatted_size(current_downloaded)}** / **{get_formatted_size(total_downloaded)}**"
+        
+        await hm.edit(
+            f"{head_text}\n{progress_bar}\n{speed_line}\n{time_line}\n{size_line}",
+            parse_mode="markdown"
+        )
+    
+    uuid = str(uuid4())
+    thumbnail = download_image_to_bytesio(data["thumb"], "thumbnail.png")
+
+    try:
+        file = await bot.send_file(
+            PRIVATE_CHAT_ID,
+            file=data["direct_link"],
+            thumb=thumbnail if thumbnail else None,
+            progress_callback=progress_bar,
+            caption=f"""
+File Name: `{data['file_name']}`
+Size: **{data["size"]}** 
+Direct Link: [Click Here](https://t.me/pom_pom_pom_terabox_bot?start={uuid})
+
+@backupbackupbackup
+""",
+            supports_streaming=True,
+            spoiler=True,
+        )
+    except telethon.errors.rpcerrorlist.WebpageCurlFailedError:
+        download = await download_file(
+            data["direct_link"], data["file_name"], progress_bar
+        )
+        if not download:
+            return await hm.edit(
+                f"Sorry! Download Failed but you can download it from [here]({data['direct_link']}).",
+                parse_mode="markdown",
+            )
+        file = await bot.send_file(
+            PRIVATE_CHAT_ID,
+            download,
+            caption=f"""
+File Name: `{data['file_name']}`
+Size: **{data["size"]}** 
+Direct Link: [Click Here](https://t.me/pom_pom_pom_terabox_bot?start={uuid})
+
+Share : @backupbackupbackup
+""",
+            progress_callback=progress_bar,
+            thumb=thumbnail if thumbnail else None,
+            supports_streaming=True,
+            spoiler=True,
+        )
+        try:
+            os.unlink(download)
+        except Exception as e:
+            print(e)
+    except Exception:
+        return await hm.edit(
+            f"Sorry! Download Failed but you can download it from [here]({data['direct_link']}).",
+            parse_mode="markdown",
+        )
+    
+    try:
+        os.unlink(download)
+    except Exception as e:
+        pass
+    
+    try:
+        await hm.delete()
+    except Exception as e:
+        print(e)
+
+    if shorturl:
+        db.set(shorturl, file.id)
+    
+    if file:
+        db.set(uuid, file.id)
+        await bot(
+            ForwardMessagesRequest(
+                from_peer=PRIVATE_CHAT_ID,
+                id=[file.id],
+                to_peer=m.chat_id,
+                top_msg_id=m.id,
+                drop_author=True,
+                background=True,
+                drop_media_captions=False,
+                with_my_score=True,
+            )
+        )
+        db.set(m.sender_id, time.monotonic(), ex=60)
+        db.set(f"check_{m.sender_id}", int(count) + 1 if count else 1, ex=7200)
+
+@bot.on(
+    events.NewMessage(
+        pattern="/broadcast (.+)",
+        incoming=True,
+        outgoing=False,
+        from_users=ADMIN_IDS,  # Specify the user IDs of admins who are allowed to use this command
+    )
+)
+async def broadcast_message(m: UpdateNewMessage):
+    message = m.pattern_match.group(1)
+    # Retrieve all users from the database
+    all_users = db.keys("user:*")
+    for user_key in all_users:
+        user_id = user_key.split(":")[-1]
+        try:
+            # Send the broadcast message to each user
+            await bot.send_message(int(user_id), message)
+        except Exception as e:
+            print(f"Failed to send message to user {user_id}: {str(e)}")
+    await m.reply("Broadcast sent successfully!")
+
+@bot.on(
+    events.NewMessage(
+        pattern="/total_users",
+        incoming=True,
+        outgoing=False,
+        from_users=ADMIN_IDS,  # Specify the user IDs of admins who are allowed to use this command
+    )
+)
+async def total_users(m: UpdateNewMessage):
+    # Retrieve all users from the database
+    all_users = db.keys("user:*")
+    total_users_count = len(all_users)
+    await m.reply(f"Total number of users: {total_users_count}")
+
+bot.start(bot_token=BOT_TOKEN)
+bot.run_until_disconnected()
+
+
 # Start and run the bot
 async def main():
     await bot.start(bot_token=BOT_TOKEN)
@@ -255,4 +474,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
