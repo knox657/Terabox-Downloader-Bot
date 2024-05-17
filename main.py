42e56e348416f6636a721f251d69e6509133265f
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-05-17 09:29:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -156,7 +156,7 @@ async def start(m: UpdateNewMessage):
         [Button.url("Click here", url=link)],
         [Button.url('How to use the bot', url=TUT_VID)]
     ]
-    await m.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", buttons=btn, protect_content=False, quote=True)
+    await m.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 hours after passing the ad.", buttons=btn, quote=True)
 
     reply_text = """
 🤖 **Hello! I am your Terabox Downloader Bot** 🤖
