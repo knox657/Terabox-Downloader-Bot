28a22bc2d0d6434a2300d3791ff2e518303a08b2
Sahil Desai <130888169+sahildesai07@users.noreply.github.com>
2024-05-17 09:18:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -76,6 +76,52 @@ async def full_userbase():
 async def del_user(user_id: int):
     await user_data.delete_one({'_id': user_id})
 
+async def get_verify_status(user_id):
+    verify = await db_verify_status(user_id)
+    return verify
+
+async def update_verify_status(user_id, verify_token="", is_verified=False, verified_time=0, link=""):
+    current = await db_verify_status(user_id)
+    current['verify_token'] = verify_token
+    current['is_verified'] = is_verified
+    current['verified_time'] = verified_time
+    current['link'] = link
+    await db_update_verify_status(user_id, current)
+
+async def get_shortlink(url, api, link):
+    shortzy = Shortzy(api_key=api, base_site=url)
+    link = await shortzy.convert(link)
+    return link
+
+def get_exp_time(seconds):
+    periods = [('days', 86400), ('hours', 3600), ('mins', 60), ('secs', 1)]
+    result = ''
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            result += f'{int(period_value)}{period_name}'
+    return result
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += f"{time_list.pop()}, "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
 bot = TelegramClient("tele", API_ID, API_HASH)
 
 db = redis.Redis(
@@ -92,8 +138,8 @@ async def start(m: UpdateNewMessage):
         user_details = json.dumps({"username": m.sender.username, "first_name": m.sender.first_name, "last_name": m.sender.last_name})
         db.set(f"user:{user_id}", user_details)
 
-    #verify_status = await get_verify_status(user_id)
-    verify_status = await db_verify_status(user_id)
+    verify_status = await get_verify_status(user_id)
+    #verify_status = await db_verify_status(user_id)
     if verify_status and verify_status['is_verified']:
         await m.reply("You are already verified. You can start using the bot.")
         return
